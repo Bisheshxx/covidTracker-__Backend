f9ec7d77d4af966b0b23f5e76cd476083c492c95
Sakriya Khadka <Khadkasakriya81@gamil.com>
2021-07-12 20:28:09 +0545

    DeprecationWarning removed

--- db/db.js
+++ db/db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 //connecting the db in mongo atlas
 mongoose.connect('mongodb+srv://admin-sakriya:khadka123@cluster0.kgbux.mongodb.net/covid',{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 })
