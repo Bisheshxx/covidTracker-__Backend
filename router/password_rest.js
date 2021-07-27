b7125d64c80d233a0947e694505a4d926bafeced
Sakriya Khadka <Khadkasakriya81@gamil.com>
2021-07-27 20:37:52 +0545

    change pw api added

--- router/password_rest.js
+++ router/password_rest.js
@@ -1,4 +1,9 @@
 const  {sendMailMessage} = require('../forget_pass/mail');
+const router = express.Router();
+const saltRounds = 10;
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require('../models/registration_model');
 
 //password rest
 
@@ -11,3 +16,22 @@ let content = {
     "task" : "Password Reset"    
 }
 sendMailMessage("Password Reset",email,content);
+
+//api for password reset
+
+router.post('/resetPassword',(req,res)=>{
+    let pid = req.body.pid;
+    let password = req.body.password;
+ 
+       bcrypt.hash(password,saltRounds,(err,hash)=>{
+            User.updateOne({"_id":pid},{"password":password}).then((result)=>{
+                return res.status(200).json({"success":true,"message":"Login with your new Password!!"});
+                }).catch((err)=>{
+                    return res.status(404).json({"success":false,"message":err});
+                    })
+                })
+
+            });
+      
+               
+    
